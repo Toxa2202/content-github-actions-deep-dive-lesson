3fd7536b2b9f747ea32fa88ab55a5b0374a01055
Anton Sv <Svyatoj@gmail.com>
2023-12-10 14:31:17 +0200

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -11,8 +11,12 @@ def lambda_handler(event, context):
     print('Starting functions\n---------------------------------------------')
 
     if event["input"] == "Hello":
-
+        
         return "World"
+        
+    if event["input"] == "Hi":
+        
+        return "Hi There"
 
     else:
 
